e7e38e1371296c11889ea102bfee28572623daab
Arasti <arastigames@gmail.com>
2023-02-18 10:56:01 +0500

    removed standard animations, added sprite animator

--- Assets/Scripts/Player/PlayerVisuals.cs
+++ Assets/Scripts/Player/PlayerVisuals.cs
@@ -1,56 +1,26 @@
 using BehavioursRectangularGraph;
 using UnityEngine;
-using UnityExtensions;
 
-[RequireComponent(typeof(Animator)), RequireComponent(typeof(SpriteRenderer))]
 public class PlayerVisuals : MonoBehaviour
 {
-    private const string k_AttackTriggerPrefix = "Attack";
-
-    private Animator m_Animator;
-
     private RectangularDirection m_PreviousDirection = RectangularDirection.Down;
 
-    private readonly int m_FirstSwordAttackAnimationID = Animator.StringToHash("SwordAttack_1");
-    private readonly int m_IDLEAnimationID = Animator.StringToHash("IDLE");
-    private readonly int m_DodgeStateAnimationID = Animator.StringToHash("DodgeAnimation");
-    private readonly int m_HorizontalAnimationID = Animator.StringToHash("Horizontal");
-    private readonly int m_VerticalAnimationID = Animator.StringToHash("Vertical");
-    private readonly int m_WalkingAnimationID = Animator.StringToHash("Walking");
-    private readonly int m_DodgeAnimationID = Animator.StringToHash("Dodge");
-
-    public bool CanDodge => m_Animator.GetCurrentAnimatorStateInfo(0).shortNameHash != m_DodgeStateAnimationID
-            && m_Animator.GetNextAnimatorStateInfo(0).shortNameHash != m_DodgeStateAnimationID;
+    public bool CanDodge => true;
 
     private void Awake()
     {
-        m_Animator = GetComponent<Animator>();
     }
 
     public void TriggerAttackAnimation(int attackIndex)
     {
-        if(attackIndex == 1)
-        {
-            m_Animator.CrossFade(m_FirstSwordAttackAnimationID, 0);
-        }
-        else
-        {
-            m_Animator.SetTrigger($"{k_AttackTriggerPrefix}{attackIndex}");
-        }
     }
 
     public void Dodge(float dodgeDiscardDelay)
     {
-        if (!CanDodge) return;
-        m_Animator.CrossFade(m_IDLEAnimationID, 0);
-        m_Animator.SetTrigger(m_DodgeAnimationID);
-        this.InvokeSecondsDelayed(() => m_Animator.ResetTrigger(m_DodgeAnimationID), dodgeDiscardDelay);
     }
 
     public void ResetAttackTriggers(int attackCount)
     {
-        for (var i = 2; i <= attackCount; i++)
-            m_Animator.ResetTrigger($"{k_AttackTriggerPrefix}{i}");
     }
 
     public void AdjustMoveVisuals(Vector2 input, bool changeDirection)
@@ -73,12 +43,7 @@ public class PlayerVisuals : MonoBehaviour
                 scale = new Vector3(Mathf.Abs(scale.x), scale.y, scale.z);
             }
             transform.localScale = scale;
-
-            m_Animator.SetFloat(m_HorizontalAnimationID, horizontalInput);
-            m_Animator.SetFloat(m_VerticalAnimationID, verticalInput);
         }
-
-        m_Animator.SetBool(m_WalkingAnimationID, input.sqrMagnitude > Mathf.Epsilon);
     }
 
     public void HandleDirection(ref float horizontalInput, ref float verticalInput)
